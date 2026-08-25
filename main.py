d0cd7c9b5da22e22e31813d13897a9b60ceae48c
GitOneJFC <jcastillO@pharoslending.com>
2026-08-25 03:38:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,33 +6,55 @@
 # See the solution video in the 100 Days of Python Course for explainations.
 
 
-from datetime import datetime
-import pandas
 import random
+import pandas as pd
+import datetime as dt
 import smtplib
 import os
 
-# import os and use it to get the Github repository secrets
+
+#GIT secrets:
+
 MY_EMAIL = os.environ.get("MY_EMAIL")
 MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
+#TODO 1: Updating birthdays.csv
+#TODO 2: Check if today matches a birthday in the birthdays.csv
+# Read data from csv
+today = dt.datetime.now()
+day = today.day
+month = today.month
+year = today.year
+
+list = pd.read_csv('birthdays.csv')
+dates_list = [(list.month[item], list.day[item]) for item in range(list.index.size)]
+
+list.insert(5, 'birthdate',dates_list,True)
+# ------------------------------------------------------------------
+rec_index = list.index[list['birthdate']==(month,day)].tolist()
+
+# ---------------------------------------------------------------------
+rec_df = list[list['birthdate'] == (month, day)]
+
+# -----------------------------------------------------------------------
+
+#TODO 3 - Letter Selection
+folder = './letter_templates/'
+birthday_msg = random.choice(['letter_1.txt', 'letter_2.txt', 'letter_3.txt'])
+
+for i in rec_index:
+    with open(folder + birthday_msg,'r') as msg:
+        text = msg.read()
+        text = text.replace('[NAME]', list.name[i])
+    print(text)
+    with smtplib.SMTP('smtp.gmail.com', 587) as connection:
         connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.login(MY_EMAIL,MY_PASSWORD)
         connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
+            from_addr = MY_EMAIL,
+            to_addrs = list.email[i],
+            msg = f"Subject: Happy Birthday!\n\n {text}"
         )
+    print("Message Sent")
