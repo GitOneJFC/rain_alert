c36ee416de7e79c0e775b3c40367f6e36259515e
GitOneJFC <jcastillO@pharoslending.com>
2026-08-25 03:44:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ import os
 
 #GIT secrets:
 
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+MY_EMAIL = os.environ.get("MY_EMAIL1")
+MY_PASSWORD = os.environ.get("MY_PASSWORD1")
 
 
 
