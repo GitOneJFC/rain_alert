326739826f0e89fcd01784c6d37f9b8bc72946a0
GitOneJFC <jcastillO@pharoslending.com>
2026-09-03 05:02:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,54 +1,84 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-import random
-import pandas as pd
-import datetime as dt
-import smtplib
+import requests
 import os
+from twilio.rest import Client
+import smtplib
 
-#GIT secrets:
-MY_EMAIL1 = os.environ.get("MY_EMAIL1")
-MY_PASSWORD1 = os.environ.get("MY_PASSWORD1")
+import smtplib
+# import json
 
-#TODO 1: Updating birthdays.csv
-#TODO 2: Check if today matches a birthday in the birthdays.csv
-# Read data from csv
-today = dt.datetime.now()
-day = today.day
-month = today.month
-year = today.year
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+API_KEY = ""
 
-list = pd.read_csv('birthdays.csv')
-dates_list = [(list.month[item], list.day[item]) for item in range(list.index.size)]
+TWILIO_SMS = 
+TWILIO_API_KEY = ""
 
-list.insert(5, 'birthdate',dates_list,True)
-# ------------------------------------------------------------------
-rec_index = list.index[list['birthdate']==(month,day)].tolist()
+MY_EMAIL = ""
+MY_PASSWORD = ""
+RECIPIENT = ""
 
-# ---------------------------------------------------------------------
-rec_df = list[list['birthdate'] == (month, day)]
 
-# -----------------------------------------------------------------------
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = ""
+auth_token = ""
 
-#TODO 3 - Letter Selection
-folder = './letter_templates/'
-birthday_msg = random.choice(['letter_1.txt', 'letter_2.txt', 'letter_3.txt'])
+MY_LAT = 40.231229
+MY_LONG = -83.036921
 
-for i in rec_index:
-    with open(folder + birthday_msg,'r') as msg:
-        text = msg.read()
-        text = text.replace('[NAME]', list.name[i])
-    print(text)
-    with smtplib.SMTP('smtp.gmail.com', 587) as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL1,MY_PASSWORD1)
-        connection.sendmail(
-            from_addr = MY_EMAIL1,
-            to_addrs = list.email[i],
-            msg = f"Subject: Happy Birthday!\n\n {text}"
-        )
-    print("Message Sent")
+RAIN_LAT = 13
+RAIN_LONG = -110
+
+URL_CURRENT = "https://api.openweathermap.org/data/2.5/weather"
+
+URL_5DAY = "https://api.openweathermap.org/data/2.5/forecast"
+
+parameters = {
+    'lat': RAIN_LAT,
+    'lon': RAIN_LONG,
+    'cnt': 4,
+    'appid': API_KEY,
+}
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#
+
+response = requests.get(URL_5DAY,params = parameters)
+response.raise_for_status()
+# print(response.status_code)
+data = response.json()
+print(f"{data}\n")
+
+print(data['list'][0]['weather'][0]['id'])
+
+rain12 = False
+
+for hour in data['list']:
+    # print(hour['weather'][0]['id'])
+    condition_code = hour['weather'][0]['id']
+    if int(condition_code) < 700:
+        rain12 = True
+# print(condition_list)
+####################################NOTIFY BY TWILIO
+if rain12:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        to="whatsapp:",
+        from_="whatsapp:",
+        body="test",
+    )
+    print(message.status)
+# "sms_internal_alerts"
+# "It's going to rain today. Remember to bring an Umbrella!☂️"
+
+# #####################NOTIFY BY EMAIL###########################
+# if rain12:
+#     with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+#         connection.starttls()
+#         connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+#         connection.sendmail(
+#             from_addr=MY_EMAIL,
+#             to_addrs='',
+#             msg = f"Subject: Weather Alert!\n\n Look Up! t's going to rain today. Remember to bring an Umbrella!"
+#         )
+
+################################NOTIFY BY WHATSAPP AND TWILIO#############################
+
+
